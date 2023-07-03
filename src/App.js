fa73d7634c78c40db1a8909a7ef79896af87a971
Luis Flores <falucho@msn.com>
2023-07-03 11:24:51 -0700

    Added graphql and storage, updated deleteNote function

--- src/App.js
+++ src/App.js
@@ -58,7 +58,7 @@ const App = ({ signOut }) => {
     event.target.reset();
   }
 
-  async function deleteNote({ id }) {
+  async function deleteNote({ id, name }) {
     const newNotes = notes.filter((note) => note.id !== id);
     setNotes(newNotes);
     await Storage.remove(name);
